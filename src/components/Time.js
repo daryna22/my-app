180b3e44bc806d74335069af19381d48f2501d25
Maksymenko Daryna <maksimenkodaryna@gmail.com>
2021-01-11 15:11:04 +0200

    Time changed

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 export function Time() {
     const clock = new Date().toLocaleTimeString();
-
+    const formatName = (user) => `${user.first} ${user.last}`;
+    const data = {
+        first: 'Daryna',
+        last: 'Maksymenko'
+    };
+    const element = formatName(data);
     return <header>
-        <h1>Hello World!</h1>
+        <h1> { element }</h1>
         <span> { clock } </span>
     </header>
 
